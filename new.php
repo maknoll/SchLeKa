9802e61e18cd925b37aa92db6f37d2d66105ee13
Martin Knoll <mk@martinknoll.org>
2011-06-23 16:20:21 +0200

    add questions form

---
+++ new.php
@@ -0,0 +1,26 @@
+<?php
+include('includes/db.php');
+
+$db = connect();
+
+//TODO
+
+disconnect($db);
+
+?>
+
+<!doctype html> 
+<html>
+ <head>
+  <meta charset="utf-8" /> 
+  <title>Schlüko Lernkarten</title>
+  <link rel="stylesheet" href="style/typoframework.css" type="text/css" /> 
+  <link rel="stylesheet" href="style/main.css" type="text/css" />
+  <script type="text/javascript" src="script/ajax.js"></script>	
+ </head>
+ <body>
+  <div>
+   <p> Eintrag erstellt </p>
+  <div>
+ </body>
+</html>
---
+++ new_form.php
@@ -0,0 +1,27 @@
+<!doctype html> 
+<html>
+ <head>
+  <meta charset="utf-8" /> 
+  <title>Schlüko Lernkarten</title>
+  <link rel="stylesheet" href="style/typoframework.css" type="text/css" /> 
+  <link rel="stylesheet" href="style/main.css" type="text/css" />
+  <script type="text/javascript" src="script/ajax.js"></script>	
+ </head>
+ <body>
+  <div>
+   
+   <form action="new.php" method="GET">
+    
+	<p>Frage</p>
+    <textarea name="question" cols="64" rows="8"> </textarea>
+
+	<p>Lösung</p>
+	<textarea name="solution" cols="64" rows="8"> </textarea>
+
+	<p><input type="submit" value="Senden"><p>
+
+   </form>
+   
+  <div>
+ </body>
+</html>
