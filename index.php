6ee44bb973ea89d2b08e2c58a4e6b0999984242a
Martin Knoll <mk@martinknoll.org>
2011-06-23 20:05:02 +0200

    piwik tracking

--- index.php
+++ index.php
@@ -39,4 +39,18 @@ disconnect($db);
   </nav>
   <div class="solution" id="solution"><?php echo(nl2br($values['solution'])); ?></div>
  </body>
+
+<!-- Piwik --> 
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.martinknoll.org/" : "http://piwik.martinknoll.org/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://piwik.martinknoll.org/piwik.php?idsite=3" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
+
 </html>
