3fb51fcc5436dd6a17dbf273f6934d4bd1bd42e5
Florian Ludwig <florian@ludwig-jahn.com>
2011-06-23 16:53:23 +0200

    Datenbankabfrage für neue Einträge

--- new.php
+++ new.php
@@ -1,26 +1,60 @@
 <?php
-include('includes/db.php');
+if(isset($_POST)) {
+	include('includes/db.php');
+	
+	
+	//form validation vars
+    $formok = true;
+    $errors = array();
+	
+    //form data
+    $question = pg_escape_string(nl2br($_POST['question']));
+    $solution = pg_escape_string(nl2br($_POST['solution']));
+    $lecture = pg_escape_string($_POST['lecture']);
+    $slide = pg_escape_string($_POST['slide']);
+	
+	if(empty($question)) {
+		$formok = false;
+		$error[] = "Du musst eine Frage angeben!";
+	} elseif(strlen($question) < 11) {
+		$formok = false;
+		$error[] = "Deine Frage sollte mindestens 10 Zeichen lang sein.";
+	}
+	
+	if(empty($solution)) {
+		$formok = false;
+		$error[] = "Du musst eine Antwort angeben!";
+	} elseif(strlen($solution) < 11) {
+		$formok = false;
+		$error[] = "Deine Antwort sollte mindestens 10 Zeichen lang sein.";
+	}
+	
+	if($formok) {
+      $db = connect();
 
-$db = connect();
+	  disconnect($db);
+	  
+      // insert into db
+      pg_query("INSERT INTO questions (question, solution, lecture, slide) VALUES ('{$question}', '{$solution}', '{$lecture}', '{$slide}')");
+	}
+	
+	//what we need to return back to our form
+    $returndata = array(
+        'posted_form_data' => array(
+            'question' => $question,
+            'solution' => $solution,
+            'lecture' => $lecture,
+            'slide' => $slide,
+        ),
+        'form_ok' => $formok,
+        'errors' => $errors
+    );
+    
+    
+    //set session variables
+    session_start();
+    $_SESSION['returndata'] = $returndata;
 
-//TODO
-
-disconnect($db);
-
-?>
-
-<!doctype html> 
-<html>
- <head>
-  <meta charset="utf-8" /> 
-  <title>Schlüko Lernkarten</title>
-  <link rel="stylesheet" href="style/typoframework.css" type="text/css" /> 
-  <link rel="stylesheet" href="style/main.css" type="text/css" />
-  <script type="text/javascript" src="script/ajax.js"></script>	
- </head>
- <body>
-  <div>
-   <p> Eintrag erstellt </p>
-  <div>
- </body>
-</html>
+    //redirect back to form
+    header('location: ' . $_SERVER['HTTP_REFERER']);
+}
